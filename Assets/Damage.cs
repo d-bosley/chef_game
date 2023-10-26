3a755da8beabe31360acd9b53bfc60632926c912
d-bosley <99742721+d-bosley@users.noreply.github.com>
2023-10-26 12:33:54 -0400

    ebf21

--- Assets/Damage.cs
+++ Assets/Damage.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Damage : MonoBehaviour
 {
+    bool larger = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,26 @@ public class Damage : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+    larger = player.transform.localScale.magnitude > 1 : true ? false;
+    }
+
+    void OnCollisionStay(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            if (!larger)
+            {
+            // Show death screen
+            }
+            
+            else
+            {
+            Vector3 scale = player.transform.localScale;
+            float shrink = Vector3.one * 1.5f;
+            scale -= shrink;
+            player.transform.localScale = scale;
+            }
+        }
+
     }
 }
