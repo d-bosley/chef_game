32f471ec605ad6479ad9118d5c535a8c60bcff5d
d-bosley <99742721+d-bosley@users.noreply.github.com>
2023-10-26 13:23:36 -0400

    ebf27

---
+++ Assets/EnemyHolder.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHolder : MonoBehaviour
+{
+    public Spawner spawnScript;
+    public Transform parent;
+    public GameObject enemy;
+    public int maxEnemy = 4;
+    private Transform[] children;
+    private int currentEnemies;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < maxEnemy; i++)
+        {
+            spawnScript.enemyClone(i);
+            enemyClone.transform.parent = parent;
+        }
+
+        children = new Transform[transform.childCount];
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            children[i] = transform.GetChild(i);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentEnemies = children.Length;
+        
+        if(currentEnemies < maxEnemy)
+        {
+            int enemyCheck = currentEnemies + 1; 
+            spawnScript.enemyClone(enemyCheck);
+            currentEnemies += 1;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+//        if(other.CompareTag("Player"))
+//        {
+//            for(int i = 0; i < 4; i++)
+//            {
+//                spawnScript.enemyClone(i);
+//                enemyClone.transform.parent = parent;
+//            }
+//        }
+    }
+}
