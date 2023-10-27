1d3f6cb11577f4ac3640d23eeceb35f9547ad0ed
d-bosley <99742721+d-bosley@users.noreply.github.com>
2023-10-26 23:35:36 -0400

    Faster collectible

--- Assets/Collectible.cs
+++ Assets/Collectible.cs
@@ -24,7 +24,7 @@ public class Collectible : MonoBehaviour
     void SetMovementDestination(Vector3 initialPosition, out Vector3 newPosition)
     {
         float amplitude = .15f; // The distance the object moves from its starting position
-        float frequency = 1.0f; // The speed of the back-and-forth motion
+        float frequency = 4.0f; // The speed of the back-and-forth motion
 
         // Calculate the new position based on a sine wave
         float yOffset = amplitude * Mathf.Sin(Time.time * frequency);
