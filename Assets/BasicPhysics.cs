8914ba8fea54649671889e1dabb5919f8ea4bbfb
d-bosley <99742721+d-bosley@users.noreply.github.com>
2023-10-26 21:02:10 -0400

    Commitotalitarian

--- Assets/BasicPhysics.cs
+++ Assets/BasicPhysics.cs
@@ -270,7 +270,7 @@ public class BasicPhysics : MonoBehaviour
     Vector3 velocity = playerBody.velocity;
     vectorSplit(velocity, out Vector3 velocityLateral, out Vector3 velocityVertical);
     float fart = 5;
-    float fartForce = fart * scale;
+    float fartForce = fart * scaleCheck;
     Vector3 fartPower = Vector3.up * fartForce;
     velocityVertical = fartPower;
     velocity = velocityLateral + velocityVertical;
