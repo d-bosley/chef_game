134dda7bf0d4cf8b9a9b05325b07740b07f2d7ae
d-bosley <99742721+d-bosley@users.noreply.github.com>
2023-10-26 23:17:25 -0400

    Collectible Script

--- Assets/Collectible.cs
+++ Assets/Collectible.cs
@@ -1,11 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 
 public class Collectible : MonoBehaviour
 {
     Vector3 here;
+    Vector3 destination;
 
     // Start is called before the first frame update
     void Start()
