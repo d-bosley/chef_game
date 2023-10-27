fc40755bab77ea17dc6866ba71cf51eebf75ec21
d-bosley <99742721+d-bosley@users.noreply.github.com>
2023-10-26 23:08:21 -0400

    Collectibles

---
+++ Assets/Collectible.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Collectible : MonoBehaviour
+{
+    Vector3 here;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        here = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetMovementDestination(here, out Vector3 finalPlace);
+        destination = finalPlace;
+        transform.position = Vector3.MoveTowards(transform.position, destination, .35f);
+    }
+
+    void SetMovementDestination(Vector3 initialPosition, out Vector3 newPosition)
+    {
+        float amplitude = 1.0f; // The distance the object moves from its starting position
+        float frequency = 1.0f; // The speed of the back-and-forth motion
+
+        // Calculate the new position based on a sine wave
+        float yOffset = amplitude * Mathf.Sin(Time.time * frequency);
+
+        // Update the object's position to move back and forth along the x-axis
+        newPosition = initialPosition + (Vector3.up * yOffset);
+    }
+}
